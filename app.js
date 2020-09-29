237bb7963c15660dcfb57e8e4854b6b541314794
vickdlaluzz <vicktordlaluzz@gmail.com>
2020-09-29 14:30:05 -0500

    Puerto configurado

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 var express = require('express');
-var exphbs  = require('express-handlebars');
- 
+var exphbs = require('express-handlebars');
+
 var app = express();
- 
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.render('home');
 });
 
-app.get('/detail', function (req, res) {
+app.get('/detail', function(req, res) {
     res.render('detail', req.query);
 });
 
 app.use(express.static('assets'));
- 
+
 app.use('/assets', express.static(__dirname + '/assets'));
- 
-app.listen(3000);
+
+app.listen(process.env.PORT || 3000);
